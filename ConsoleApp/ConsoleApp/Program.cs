b08ac76e602cff5131e9de0f2fa34b9e9dd397cf
justinjenecke <justinjenecke@gmail.com>
2022-06-08 21:39:20 +0200

    creates menu interface using do-while

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -5,6 +5,59 @@ namespace ConsoleApp {
 
         static void Main(string[] args) {
 
+            string choice;
+
+            string[] tweetList = new string[5];
+            int numTweets = 0;
+
+            Console.WriteLine("========== Twitter App ===========");
+
+            do {
+                Console.WriteLine(" --- Menu --- ");
+                Console.WriteLine("\n" +
+                    "1) View all Tweets \n" +
+                    "2) Post new Tweet \n" +
+                    "3) Exit \n");
+
+                Console.Write("Please select an option from the menu: (1-3)\n");
+                choice = Console.ReadLine();
+                Console.WriteLine("\n--- Your Choice: {0} ---\n", choice);
+
+                switch (choice) {
+                    case "1":
+                        for (int i = 0; i < numTweets; i++) {
+                            if(tweetList[i] != null)
+                                Console.WriteLine("Tweet no{0}: '{1}'", i+1, tweetList[i]);
+                        }
+                        break;
+
+                    case "2":
+                        if (numTweets <= tweetList.Length){
+                            Console.Write("Please enter your new Tweet: ");
+                            string newTweet = Console.ReadLine();
+
+                            if (newTweet.Length > 50) {
+                                Console.WriteLine("Error, Tweets cannot have more than 50 characters.. \n exiting operation...");
+                                continue;
+                            }
+                            else {
+                                tweetList[numTweets] = newTweet;
+                                numTweets++;
+                                Console.WriteLine("Tweet posted successfully.");
+                            }
+                        }
+                        else {
+                            continue;
+                        }
+                        break;
+                }
+
+            } while (choice != "3");
+
+            Console.WriteLine("Exiting application.");
+            Console.ReadLine();
+
+            /*
             Console.WriteLine("How many tweets to add? [Max 5]");
             int noTweets = int.Parse( Console.ReadLine() );
 
@@ -31,17 +84,6 @@ namespace ConsoleApp {
                 Console.WriteLine("Tweet {0}: {1}", i + 1, tweetList[i]);
             }
 
-            Console.ReadLine();
-            /*
-            string tweet = "saluton mundo";
-            int tweetLength = tweet.Length;
-
-            Console.WriteLine("Registering Tweet.. \n Posting Tweet to your timeline... \n Tweet posted successfully.... \n");
-            Console.Write(
-                "Your Tweet is {0} and its lenght is {1} characters",
-                tweet,
-                tweetLength
-                );
             Console.ReadLine();
             */
         }
